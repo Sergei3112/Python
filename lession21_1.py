b3fe03bfcc276e9aa892f7839a224c1c999e8244
serg <yyy.tre@yandex.ru>
2022-07-28 14:12:42 +0500

    задача 21_1 к семинару 5.

--- lession21_1.py
+++ lession21_1.py
@@ -1,8 +1,14 @@
+# Напишите программу, удаляющую из текста все слова, содержащие "абв".
+
+
 incoming_text = 'абвлабфбв слова абвцукв убраны алоабвабв :)'
 
+
 def del_words(incoming_text):
-    incoming_text = list(filter(lambda x: 'абв' not in x, incoming_text.split()))
+    incoming_text = list(
+        filter(lambda x: 'абв' not in x, incoming_text.split()))
     return " ".join(incoming_text)
+
 
 incoming_text = del_words(incoming_text)
 print(incoming_text)
